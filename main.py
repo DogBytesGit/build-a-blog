dff8a28baed0cbfa022898d0ed4490a27c0129c8
John Sirois <JPSirois@verizon.net>
2017-02-10 13:23:43 -0500

    added jinj2 in app.yaml

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import os
 import webapp2
-import jinja2
+#import jinja
 
 from google.appengine.ext import db
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     #This class 
@@ -38,21 +21,21 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):    
     def get(self):
         #This displays the Main Page
-        self.render("main.html")
+        self.render(main.html)
 
-#class BlogHandler(webapp2.RequestHanlder):
-#    def get(self):
-#        #Filters query results to display 5 most recent posts        
-#        self.response.write('Displays the 5 most recent posts')
+class BlogHandler(webapp2.RequestHanlder):
+    def get(self):
+        #Filters query results to display 5 most recent posts        
+        self.response.write('Displays the 5 most recent posts')
 
-#class NewPostHandler(db.Model):
-#    #Invoked to add a new post
-#    subject = db.StringProperty(required = True)
-#    content = db.TextProperty(required = True)
-#    created = db.DateTimeProperty(auto_now_add = True)
-#    modified = db.DateTimeProperty(auto_now = True)
-#    #author = db.StringProperty(required = True)
-#    #rating = db.IntegerProperty(required = False)
+class NewPostHandler(db.Model):
+    #Invoked to add a new post
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    modified = db.DateTimeProperty(auto_now = True)
+    #author = db.StringProperty(required = True)
+    #rating = db.IntegerProperty(required = False)
 
 #    def render(self):
 #        pass
